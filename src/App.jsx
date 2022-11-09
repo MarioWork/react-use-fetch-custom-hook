ab23667e82707d8671f7c0b6fbf5bdaf4cd5d5a2
MarioWork <mariovieira.work@hotmail.com>
2022-11-09 20:27:10 -0100

    used useFetch hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import { useState } from "react";
+import useFetch from "./hooks/usefetch";
 
 function App() {
-  return <></>;
+  const url = `http://jsonplaceholder.typicode.com/posts`;
+  const { isLoading, data, error } = useFetch(url);
+
+  if (isLoading) {
+    return <h1>Loading ...</h1>;
+  }
+
+  if (error) {
+    return <h1>There was an error...</h1>;
+  }
+
+  return (
+    <div>
+      {data?.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
